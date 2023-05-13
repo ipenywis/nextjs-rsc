68f1e3ab0da457bea2c239755bf5f0bfc531dec7
Islem Maboud <islempenywis@gmail.com>
2023-05-13 15:11:07 +0100

    Allow to add comments through server actions

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,10 +1,12 @@
 import Tweet from "@/components/tweet";
 import prisma from "@/lib/prisma";
 
+const wait = (ms: number) => new Promise((rs) => setTimeout(rs, ms));
+
 export default async function Page() {
   const tweets = await prisma.tweet.findMany({ include: { author: true } });
 
-  console.log("Tweets from DB: ", tweets);
+  await wait(5000);
 
   return tweets.map((tweet: any, idx: number) => (
     <Tweet key={idx} tweet={tweet} />
--- components/tweet.tsx
+++ components/tweet.tsx
@@ -1,69 +1,54 @@
-import {
-  FaRegComment,
-  FaRetweet,
-  FaRegHeart,
-  FaRegShareSquare,
-} from "react-icons/fa";
-
-import { AiOutlineRetweet } from "react-icons/ai";
-
-import { FiShare } from "react-icons/fi";
-
 import { Prisma } from "@prisma/client";
 import { timeAgo } from "@/lib/time-ago";
+import { TweetActions } from "./tweetActions";
+import Link from "next/link";
+import { CommentsList } from "./commentsList";
+import { AddComment } from "./addComment";
 
-const TweetActions = () => (
-  <div className="flex space-x-6 mt-3 text-gray-500 justify-evenly text-xl w-10/12 transition-all duration-300">
-    <div className="cursor-pointer flex items-center space-x-1 group hover:text-blue-500 transition-colors duration-200">
-      <FaRegComment className="group-hover:text-blue-500" />
-      {/* <span>{0}</span> */}
-    </div>
-    <div className="cursor-pointer flex items-center space-x-1 group hover:text-green-500 transition-colors duration-200">
-      <AiOutlineRetweet className="group-hover:text-green-500" />
-      {/* <span>{0}</span> */}
-    </div>
-    <div className="cursor-pointer flex items-center space-x-1 group hover:text-red-500 transition-colors duration-200">
-      <FaRegHeart className="group-hover:text-red-500" />
-      {/* <span>{0}</span> */}
-    </div>
-    <div className="cursor-pointer flex items-center space-x-1 group hover:text-blue-500 transition-colors duration-200">
-      <FiShare className="group-hover:text-blue-500" />
-    </div>
-  </div>
-);
-
-type TweetWithAuthor = Prisma.TweetGetPayload<{
-  include: { author: true };
+type TweetWithAuthorAndComments = Prisma.TweetGetPayload<{
+  include: { author: true; comments: { include: { author: true } } };
 }>;
 
 interface TweetProps {
-  tweet: TweetWithAuthor;
+  tweet: TweetWithAuthorAndComments;
+  isTweetPage?: boolean;
 }
 
-export default function Tweet({ tweet }: TweetProps) {
+const WrapperLink = ({ children, isTweetPage, href }) =>
+  isTweetPage ? children : <Link href={href}>{children}</Link>;
+
+export default function Tweet({ tweet, isTweetPage }: TweetProps) {
   return (
-    <div className="flex flex-col border-b border-gray-600 p-4 max-w-2xl">
-      <div className="flex space-x-3">
-        <img
-          className="h-14 w-14 rounded-full"
-          src="https://xsgames.co/randomusers/avatar.php?g=male"
-          alt="User profile"
-        />
-        <div className="flex-1">
-          <div className="flex items-center text-lg">
-            <h4 className="font-bold">{tweet?.author?.name}</h4>
-            <p className="ml-2 text-gray-500">@{tweet.author.name}</p>
-            <span className="mx-1 text-gray-500">·</span>
-            <p className="text-gray-500">
-              {timeAgo(new Date(tweet.createdAt))}
-            </p>
-          </div>
-          <p className="mt-2 text-white font-base text-2xl">{tweet.text}</p>
-          <div className="flex space-x-6 mt-3 text-gray-500">
-            <TweetActions />
+    <WrapperLink href={`/timeline/${tweet.id}`} isTweetPage={isTweetPage}>
+      <div className="flex flex-col border-b border-gray-600 p-4 max-w-2xl">
+        <div className="flex space-x-3">
+          <img
+            className="h-14 w-14 rounded-full"
+            src={`https://xsgames.co/randomusers/assets/avatars/male/${tweet.id}.jpg`}
+            alt="User profile"
+          />
+          <div className="flex-1">
+            <div className="flex items-center text-lg">
+              <h4 className="font-bold">{tweet?.author?.name}</h4>
+              <p className="ml-2 text-gray-500">@{tweet.author.name}</p>
+              <span className="mx-1 text-gray-500">·</span>
+              <p className="text-gray-500">
+                {timeAgo(new Date(tweet.createdAt))}
+              </p>
+            </div>
+            <p className="mt-2 text-white font-base text-2xl">{tweet.text}</p>
+            <div className="flex space-x-6 mt-3 text-gray-500">
+              <TweetActions />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {isTweetPage && (
+        <div className="flex flex-col max-w-2xl w-full">
+          <CommentsList comments={tweet.comments} />
+          <AddComment tweet={tweet} />
+        </div>
+      )}
+    </WrapperLink>
   );
 }
